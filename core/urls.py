b1b5da4cc9a4f0aa0fc561e44a5dd14626c37c6c
avishkararjan <avishkar2108@gmail.com>
2025-07-27 12:46:54 +0530

    need to add some content and styling

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('nebula/', views.nebula, name='nebula')
 
 ]
 
--- core/views.py
+++ core/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from .models import Return
 
 def home(request):
-    returns = Return.objects.all().order_by('-date')  # latest first
+    # returns = Return.objects.all().order_by('-date')  # latest first
+    returns = []
+    """
+    Date	Strategy	Return (%)	Notes
+    July 27, 2025	Nebula-Mark1	12.00	The return %age should be dynamically rendered on the page.
+    """
     return render(request, 'home.html', {'returns': returns})
 
 def about(request):
     return render(request, 'about.html')
 
+def nebula(request):
+    return render(request, 'nebula.html')

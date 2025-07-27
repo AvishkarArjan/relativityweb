f87552db599a6967178f8fda09bd2583aa9de88e
avishkararjan <avishkar2108@gmail.com>
2025-07-27 14:16:08 +0530

    images are a little heavy but wtvr

--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import Return
 
 def home(request):
-    # returns = Return.objects.all().order_by('-date')  # latest first
-    returns = []
+    returns = Return.objects.all().order_by('-date')  # latest first
+    # returns = []
     """
     Date	Strategy	Return (%)	Notes
     July 27, 2025	Nebula-Mark1	12.00	The return %age should be dynamically rendered on the page.
